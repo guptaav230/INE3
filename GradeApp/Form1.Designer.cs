58288d0727371f5eee695fa2e6c2914e2fb9b75d
Avinash Gupta <Avinash Gupta@AVINASH-GUPTA>
2018-05-25 09:55:07 -0400

    version1.1

--- GradeApp/Form1.Designer.cs
+++ GradeApp/Form1.Designer.cs
@@ -34,7 +34,6 @@
             this.letterGradeTb = new System.Windows.Forms.TextBox();
             this.calculateBtn = new System.Windows.Forms.Button();
             this.exitBtn = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblNumber
@@ -93,21 +92,11 @@
             this.exitBtn.UseVisualStyleBackColor = true;
             this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(27, 191);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // LetterGradefrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.exitBtn);
             this.Controls.Add(this.calculateBtn);
             this.Controls.Add(this.letterGradeTb);
@@ -129,7 +118,6 @@
         private System.Windows.Forms.TextBox letterGradeTb;
         private System.Windows.Forms.Button calculateBtn;
         private System.Windows.Forms.Button exitBtn;
-        private System.Windows.Forms.Button button1;
     }
 }
 
